c57ce9eb38b814d7737d8d547e212ecba249c587
jaypark <pjhyun1990@gmail.com>
2023-05-11 17:01:11 +0900

    refactor di 적용

--- image_search/lib/main.dart
+++ image_search/lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:image_search/data/data_source/pixabay_api.dart';
-import 'package:image_search/data/repository/photo_api_repository_impl.dart';
-import 'package:image_search/domain/use_case/get_photos_use_case.dart';
+import 'package:image_search/di/provider_setup.dart';
 import 'package:image_search/presentation/home/home_screen.dart';
-import 'package:image_search/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: globalProviders,
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,14 +17,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ChangeNotifierProvider(
-          create: (_) => HomeViewModel(GetPhotosUseCase(
-              PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-          child: const HomeScreen(),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
