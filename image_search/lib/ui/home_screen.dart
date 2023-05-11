30b81c503a019b3435f6ab6b46d04a055c2f9f3e
jaypark <pjhyun1990@gmail.com>
2023-05-11 09:58:38 +0900

    feat base project

---
+++ image_search/lib/ui/home_screen.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_search/ui/widget/photo_widget.dart';
+
+import '../model/photo.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Photo> _photos = [];
+  final _controller = TextEditingController();
+
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(Uri.parse(
+        "https://pixabay.com/api/?key=36248505-691a3381d94fad8b197cd0d86&q=$query&image_type=photo&pretty=true"));
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "이미지 검색 앱",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () async {
+                    final photos = await fetch(_controller.text);
+                    setState(() {
+                      _photos = photos;
+                    });
+                  },
+                  icon: const Icon(Icons.search),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: _photos.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                itemBuilder: (context, index) {
+                  return PhotoWidget(
+                    photo: _photos[index],
+                  );
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
