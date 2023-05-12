3500e1773515d0064fac166301fc09be95d19b0a
jaypark <pjhyun1990@gmail.com>
2023-05-13 01:03:07 +0900

    feat Hero widget

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -29,25 +29,28 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      offset: const Offset(10, 10),
-                      blurRadius: 15,
-                    ),
-                  ],
-                ),
-                width: 250,
-                child: Image.network(
-                  webtoonModel.thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  },
+              Hero(
+                tag: webtoonModel.id,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        offset: const Offset(10, 10),
+                        blurRadius: 15,
+                      ),
+                    ],
+                  ),
+                  width: 250,
+                  child: Image.network(
+                    webtoonModel.thumb,
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    },
+                  ),
                 ),
               ),
             ],
--- toonflix/lib/widgets/webtoon_widget.dart
+++ toonflix/lib/widgets/webtoon_widget.dart
@@ -15,31 +15,34 @@ class WebtoonWidget extends StatelessWidget {
           context,
           MaterialPageRoute(
             builder: (_) => DetailScreen(webtoonModel: webtoonModel),
-            fullscreenDialog: true,
+            // fullscreenDialog: true,
           ),
         );
       },
       child: Column(
         children: [
-          Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: const Offset(10, 10),
-                  blurRadius: 15,
-                ),
-              ],
-            ),
-            width: 250,
-            child: Image.network(
-              webtoonModel.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+          Hero(
+            tag: webtoonModel.id,
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    offset: const Offset(10, 10),
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+              width: 250,
+              child: Image.network(
+                webtoonModel.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
           ),
           const SizedBox(
