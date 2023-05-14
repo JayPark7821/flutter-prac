678beada6b04bbb1488700908973eca2bcf92ef7
jaypark <pjhyun1990@gmail.com>
2023-05-14 23:05:12 +0900

    feat repository 보완 & csv parser 개발

--- stock_app/lib/data/repository/stock_repository_impl.dart
+++ stock_app/lib/data/repository/stock_repository_impl.dart
@@ -3,12 +3,14 @@ import 'package:stock_app/domain/model/company_listing.dart';
 import 'package:stock_app/domain/repository/stock_repository.dart';
 import 'package:stock_app/util/result.dart';
 
+import '../csv/company_listings_parser.dart';
 import '../source/local/stock_dao.dart';
 import '../source/remote/stock_api.dart';
 
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api;
   final StockDao _dao;
+  final _parser = CompanyListingsParser();
 
   StockRepositoryImpl(this._api, this._dao);
 
@@ -28,8 +30,15 @@ class StockRepositoryImpl implements StockRepository {
 
     // 리모트
     try {
-      final remoteListings = await _api.getListings();
-      return Result.success([]);
+      final response = await _api.getListings();
+      final remoteListings = await _parser.parse(response.body);
+
+      await _dao.clearCompanyListrings();
+
+      await _dao.insertCompanyListrings(
+          remoteListings.map((e) => e.toCompanyListingEntity()).toList());
+
+      return Result.success(remoteListings);
     } catch (e) {
       return Result.error(Exception("데이터 로드 실패"));
     }
--- stock_app/lib/data/source/remote/stock_api.dart
+++ stock_app/lib/data/source/remote/stock_api.dart
@@ -3,13 +3,13 @@ import 'package:http/http.dart' as http;
 class StockApi {
   static const baseUrl = "https://alphavantage.co/";
   static const apiKey = "F2W8LP7ZDI1C1L32";
-  final http.Client client;
+  final http.Client _client;
 
-  StockApi(this.client);
+  StockApi({http.Client? client}) : _client = client ?? http.Client();
 
   Future<http.Response> getListings({String apiKey = apiKey}) async {
-    return await client.get(
-      Uri.parse("${baseUrl}query?function-LISTING_STATUS&apikey=$apiKey"),
+    return await _client.get(
+      Uri.parse("${baseUrl}query?function=LISTING_STATUS&apikey=$apiKey"),
     );
   }
 }
