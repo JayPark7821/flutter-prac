0c8a0bb7951aa4f5b8f2f5a89313e73090813a04
jaypark <pjhyun1990@gmail.com>
2023-05-16 14:31:53 +0900

    feat implementing detail page 2

--- food_delivery/lib/restaurant/view/restaurant_detail_screen.dart
+++ food_delivery/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/common/layout/default_layout.dart';
-import 'package:food_delivery/restaurant/component/restaurant_card.dart';
+
+import '../../common/layout/default_layout.dart';
+import '../../product/component/product_card.dart';
+import '../component/restaurant_card.dart';
 
 class RestaurantDetailScreen extends StatelessWidget {
   const RestaurantDetailScreen({Key? key}) : super(key: key);
@@ -9,20 +11,57 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: "test",
-            tags: ["test"],
-            ratingsCount: 12,
-            deliveryTime: 12,
-            deliveryFee: 12,
-            ratings: 12,
-            isDetail: true,
-            detail: "테스트 디테일",
-          )
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: "test",
+        tags: ["test"],
+        ratingsCount: 12,
+        deliveryTime: 12,
+        deliveryFee: 12,
+        ratings: 12,
+        isDetail: true,
+        detail: "테스트 디테일",
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate((context, index) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: ProductCard(),
+          );
+        }, childCount: 10),
+      ),
+    );
+  }
+
+  Widget renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
