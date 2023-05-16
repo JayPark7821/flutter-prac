3b824dffa5bfb5b6e99faeae9aca2a4cbbfd3602
jaypark <pjhyun1990@gmail.com>
2023-05-16 23:41:20 +0900

    provider prac

--- flutter_provider/lib/screen/state_provider_screen.dart
+++ flutter_provider/lib/screen/state_provider_screen.dart
@@ -1,16 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/riverpod/state_provider_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../layout/default_layout.dart';
 
-class StateProviderScreen extends StatelessWidget {
+class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
     return DefaultLayout(
       title: 'StateProviderScreen',
-      body: Column(
-        children: [],
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => _NextScreen()),
+                );
+              },
+              child: Text('next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            )
+          ],
+        ),
       ),
     );
   }
