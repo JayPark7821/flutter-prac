717342255ee9a5fe06232fd062a4c4336e382416
jaypark <pjhyun1990@gmail.com>
2023-05-17 22:45:42 +0900

    riverpod v2 code generation keepalive

--- flutter_provider/lib/screen/home_screen.dart
+++ flutter_provider/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/screen/auto_dispose_modifier_screen.dart';
+import 'package:flutter_provider/screen/code_generation_screen.dart';
 import 'package:flutter_provider/screen/family_modifier_screen.dart';
 import 'package:flutter_provider/screen/future_provider_screen.dart';
 import 'package:flutter_provider/screen/listen_provider_screen.dart';
@@ -109,6 +110,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('ProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const CodeGenerationScreen(),
+                ),
+              );
+            },
+            child: const Text('CodeGenerationScreen'),
           ),
         ],
       ),
