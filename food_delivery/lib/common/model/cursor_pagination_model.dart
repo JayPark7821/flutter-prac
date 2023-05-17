804665258c28934c28b7f8115912b77bf7ce0086
jaypark <pjhyun1990@gmail.com>
2023-05-18 01:16:19 +0900

    feat define pagination state

--- food_delivery/lib/common/model/cursor_pagination_model.dart
+++ food_delivery/lib/common/model/cursor_pagination_model.dart
@@ -2,10 +2,20 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({required this.message});
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(
   genericArgumentFactories: true,
 )
-class CursorPaginationModel<T> {
+class CursorPaginationModel<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -28,4 +38,18 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+class CursorPaginationRefetching extends CursorPaginationModel {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+class CursorPaginationFetchingMore extends CursorPaginationModel {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
