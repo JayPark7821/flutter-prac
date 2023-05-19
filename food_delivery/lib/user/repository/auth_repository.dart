26600c59f76b6086494ae833cc67ffaf7abab842
jaypark <pjhyun1990@gmail.com>
2023-05-20 00:20:43 +0900

    feat user repository

---
+++ food_delivery/lib/user/repository/auth_repository.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:food_delivery/common/model/login_response.dart';
+import 'package:food_delivery/common/model/token_response.dart';
+import 'package:food_delivery/common/utils/data_utils.dart';
+
+class AuthRepository {
+  final String baseUrl;
+  final Dio dio;
+
+  AuthRepository({
+    required this.baseUrl,
+    required this.dio,
+  });
+
+  Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async {
+    final serialized = DataUtils.pathToUrl('$username:$password');
+    final response = await dio.post(
+      '$baseUrl/login',
+      options: Options(
+        headers: {'authorization': 'Basic $serialized'},
+      ),
+    );
+
+    return LoginResponse.fromJson(response.data);
+  }
+
+  Future<TokenResponse> token() async {
+    final response = await dio.post('$baseUrl/token',
+        options: Options(
+          headers: {'refreshToken': 'true'},
+        ));
+    return TokenResponse.fromJson(response.data);
+  }
+}
