1836618b93a76405f60da50dd79d44908b34a472
jaypark <pjhyun1990@gmail.com>
2023-05-22 09:06:30 +0900

    order repository 작업

---
+++ food_delivery/lib/order/repository/order_repository.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_delivery/common/dio/dio.dart';
+import 'package:food_delivery/order/model/post_order_body.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../common/const/data.dart';
+import '../model/order_model.dart';
+
+part 'order_repository.g.dart';
+
+final OrderRepositoryProvider = Provider<OrderRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return OrderRepository(dio, baseUrl: 'http://$ip/order');
+});
+
+@RestApi()
+abstract class OrderRepository {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @POST('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<OrderModel> postOrder({
+    @Body() required PostOrderBody body,
+  });
+}
